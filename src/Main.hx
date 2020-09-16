3668309e78bf3eae46acc41a3110a3c8174622e4
inc0der <inc0der.code@gmail.com>
2020-09-16 15:28:21 -0300

    Update style by running hxformatter

--- src/Main.hx
+++ src/Main.hx
@@ -11,10 +11,11 @@ import utils.Fn;
 
 class Main {
   public static function main() {
-     /**
+    /**
       Window_GameEnd
     **/
     Comment.title('Window_GameEnd');
+
     Fn.setPrProp(Window_GameEnd, 'makeCommandList', () -> {
       var self: Window_GameEnd = Fn.self;
       self.addCommand(TextManager.toTitle, 'toTitle', true);
@@ -33,28 +34,24 @@ class Main {
       Scene_GameEnd
     **/
     Comment.title('Scene_GameEnd');
-     var oldCreate: JsFn =  Fn.getPrProp(Scene_GameEnd, 'create');
+
+    var oldCreate: JsFn = Fn.getPrProp(Scene_GameEnd, 'create');
     Fn.setPrProp(Scene_GameEnd, 'create', () -> {
       var self: Dynamic = Fn.self;
       oldCreate.call(self);
       self.createConfirmWindow();
     });
 
-    var oldCreateCommandWindow: JsFn =  Fn.getPrProp(Scene_GameEnd, 'createCommandWindow');
+    var oldCreateCommandWindow: JsFn = Fn.getPrProp(Scene_GameEnd, 'createCommandWindow');
     Fn.setPrProp(Scene_GameEnd, 'createCommandWindow', () -> {
       var self: Scene_GameEnd = Fn.self;
       oldCreateCommandWindow.call(self);
-      self.__commandWindow.setHandler("toDesktop", untyped self.quit2desktop.bind(self));
+      self.__commandWindow.setHandler('toDesktop', untyped self.quit2desktop.bind(self));
     });
 
     Fn.setPrProp(Scene_GameEnd, 'createConfirmWindow', () -> {
       var self: Scene_GameEnd = Fn.self;
-      var rect = new Rectangle(
-        self.__commandWindow.x,
-        self.__commandWindow.y,
-        250,
-        self.calcWindowHeight(2, true)
-      );
+      var rect = new Rectangle(self.__commandWindow.x, self.__commandWindow.y, 250, self.calcWindowHeight(2, true));
       var confirmWindow = new Window_Confirm(rect);
       untyped self._confirmWindow = confirmWindow;
       confirmWindow.setHandler('ok', untyped self.onConfirm.bind(self));
@@ -79,7 +76,6 @@ class Main {
       }
 
       self._confirmWindow.activate();
-      
     });
   }
 }
