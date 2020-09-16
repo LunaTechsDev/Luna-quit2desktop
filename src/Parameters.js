d63089247c50cd64659754023fb3e3d4aa7eeff1
inc0der <inc0der.code@gmail.com>
2020-09-16 15:10:01 -0300

    Initial commit

---
+++ src/Parameters.js
@@ -0,0 +1,10 @@
+ /*:
+ * @plugindesc Adds options to quit directly to desktop in the main menu, title or game exit scenes.
+ * <Luna_Quit2Desktop>
+ *
+ * @target MZ MV
+ * @author LunaTechs | inc0der
+ * @url https://lunatechs.dev/plugins/luna-quit2desktop/
+ *
+ *
+ */
---
+++ src/windows/Window_Confirm.hx
@@ -0,0 +1,20 @@
+package windows;
+
+import rm.windows.Window_Command;
+import rm.core.Rectangle;
+
+@:native('Window_Confirm')
+class Window_Confirm extends Window_Command {
+  public function new(rect: Rectangle) {
+    super(rect);
+  }
+
+  public override function refresh() {
+    super.refresh();
+  }
+
+  public override function makeCommandList() {
+    addCommand('Yes', 'yes', true);
+    addCommand('No', 'no', true);
+  }
+}
