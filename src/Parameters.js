95cc2a34d4ae84fbc2d02eefab50b4e71f34d9f3
inc0der <inc0der.code@gmail.com>
2020-09-16 18:29:37 -0300

    Add parameters

--- src/Parameters.js
+++ src/Parameters.js
@@ -1,10 +1,37 @@
  /*:
- * @plugindesc Adds options to quit directly to desktop in the main menu, title or game exit scenes.
+ * @plugindesc Adds options to quit directly to desktop in the title or game exit scenes.
  * <Luna_Quit2Desktop>
  *
  * @target MZ MV
  * @author LunaTechs | inc0der
  * @url https://lunatechs.dev/plugins/luna-quit2desktop/
+ * 
+ * @param showOnGameEnd
+ * @text Show on GameEnd Scene
+ * @type boolean
+ * @desc Do you want a quit command on the GameEnd scene?
+ * @default true
+ * @on Show
+ * @off Hide
+ * 
+ * @param gameEndText
+ * @text Game End Command Text
+ * @type text
+ * @desc The command text that appears on the GameEnd scene.
+ * @default To Desktop
  *
+ * @param showOnTitle
+ * @text Show on Title Scene
+ * @type boolean
+ * @desc Do you want a quit command on the Title scene?
+ * @default true
+ * @on Show
+ * @off Hide
+ *
+ * @param titleCommandText
+ * @text Title Command Text
+ * @type text
+ * @desc The command text that appears on the Title scene.
+ * @default Quit
  *
  */
